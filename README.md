6c9e05908a4d604673aec2c11369c4347b85f5ff
saiftanoli <saiftanoli77@gmail.com>
2022-01-11 15:52:43 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-w6fusj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-w6fusj)
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <signupform />
+</template>
+
+<script>
+import signupform from './components/signupform.vue'
+
+export default {
+ 
+  components: {signupform }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
